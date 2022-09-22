c196993a2f2339a221c5f8e8d7fd12c1147f27de
heemin <gmlals9628@naver.com>
2022-09-23 00:41:45 +0900

    firebase auth완료

--- app/src/main/java/com/example/Colorful_Daegu/MainActivity.java
+++ app/src/main/java/com/example/Colorful_Daegu/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.Colorful_Daegu;
 
+import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.Colorful_Daegu.R;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
 public class MainActivity extends AppCompatActivity {
+
+    // FirebaseUI 활동 결과 계약의 콜백을 등록하는 ActivityResultLauncher
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    // Choose authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            /*new AuthUI.IdpConfig.EmailBuilder().build(),*/
+            new AuthUI.IdpConfig.GoogleBuilder().build());
+
+    // Create and launch sign-in intent
+    Intent signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            //.setLogo(R.drawable.img)      // Set logo drawable
+            //.setTheme(R.style.themes)      // Set theme
+            .build();
+
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Log.d("LOGIN", "Login success");
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            Log.d("LOGIN", "Login failed!!");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signInLauncher.launch(signInIntent);
     }
 }
