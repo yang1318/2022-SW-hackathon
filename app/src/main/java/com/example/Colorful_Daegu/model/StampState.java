c743277a47a3a432547d0959b058bcbcc9940af7
dahyun0917 <dahyun0917@naver.com>
2022-09-24 05:53:17 +0900

    스탬프현황 디비구조 수정

--- app/src/main/java/com/example/Colorful_Daegu/model/StampState.java
+++ app/src/main/java/com/example/Colorful_Daegu/model/StampState.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class StampState {
-    private HashMap<String, HashMap<String,ArrayList<Integer>>> stampState;
+    private HashMap<String, ArrayList<Integer>> stampState;
 
     public StampState(){}
 
-    public StampState(HashMap<String, HashMap<String, ArrayList<Integer>>> stampState) {
+    public StampState(HashMap<String, ArrayList<Integer>> stampState) {
         this.stampState = stampState;
     }
 
-    public HashMap<String, HashMap<String, ArrayList<Integer>>> getStampState() {
+    public HashMap<String, ArrayList<Integer>> getStampState() {
         return stampState;
     }
 
-    public void setStampState(HashMap<String, HashMap<String, ArrayList<Integer>>> stampState) {
+    public void setStampState(HashMap<String, ArrayList<Integer>> stampState) {
         this.stampState = stampState;
     }
 }
