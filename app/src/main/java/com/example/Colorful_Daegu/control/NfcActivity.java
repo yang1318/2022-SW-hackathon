d8a0ee0228421e232f83722dfdd705fee9aa3425
heemin <gmlals9628@naver.com>
2022-09-24 04:36:13 +0900

    nfc초반작업 및 로그인&회원가입 정보 저장

--- app/src/main/java/com/example/Colorful_Daegu/control/NfcActivity.java
+++ app/src/main/java/com/example/Colorful_Daegu/control/NfcActivity.java
@@ -1,7 +1,74 @@
 package com.example.Colorful_Daegu.control;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
 import com.example.Colorful_Daegu.R;
 
-public class NfcActivity {
+public class NfcActivity extends Activity {
+    private NfcAdapter nfcAdapter;
+    private PendingIntent pendingIntent;
+    private static String tagNum=null;
+    private TextView tagDesc;
 
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc);
+        tagDesc= (TextView) findViewById(R.id.spotname);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        Intent intent = new Intent(this,getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        //pendingIntent = pendingIntent.getActivity(this,0,intent(this,).addFlags,0);
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if(intent.getAction() == NfcAdapter.ACTION_NDEF_DISCOVERED){
+            Tag tag =intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            if(tag != null) {
+                byte[] tagId = tag.getId();
+                tagDesc.setText("TagID: " + toHexString(tagId));
+                System.out.println("@@@@@@@@"+toHexString(tagId));
+                tagNum = toHexString(tagId);
+            }
+        }
+
+    }
+    public static final String CHARS = "0123456789ABCDEF";
+    public static String toHexString(byte[] data) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < data.length; ++i) {
+            sb.append(CHARS.charAt((data[i] >> 4) & 0x0F)).append(
+                    CHARS.charAt(data[i] & 0x0F)
+            );
+        }
+        return sb.toString();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        if(nfcAdapter != null){
+//            nfcAdapter
+//                    .enableForegroundDispatch(this,pendingIntent,null,null);
+//        }
+    }
+
+    @Override
+    protected void onPause() {
+        if(nfcAdapter != null){
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+        super.onPause();
+    }
 }
--- app/src/main/java/com/example/Colorful_Daegu/control/StartActivity.java
+++ app/src/main/java/com/example/Colorful_Daegu/control/StartActivity.java
@@ -5,21 +5,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.Colorful_Daegu.MainActivity;
 import com.example.Colorful_Daegu.R;
+import com.example.Colorful_Daegu.model.User;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,7 +34,7 @@ public class StartActivity extends AppCompatActivity {
     //DatabaseReference는 데이터베이스의 특정 위치로 연결하는 거라고 생각하면 된다.
     //현재 연결은 데이터베이스에만 딱 연결해놓고
     //키값(테이블 또는 속성)의 위치 까지는 들어가지는 않은 모습이다.
-    private DatabaseReference databaseReference = database.getReference();
+    private DatabaseReference mDatabase;
 
     // FirebaseUI 활동 결과 계약의 콜백을 등록하는 ActivityResultLauncher
     private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
@@ -57,22 +63,36 @@ public class StartActivity extends AppCompatActivity {
 
     private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
         IdpResponse response = result.getIdpResponse();
-        if (result.getResultCode() == RESULT_OK) {
-            // Successfully signed in
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            Log.d("LOGIN", user.getDisplayName());
+        if (result.getResultCode() == RESULT_OK) { /*   로그인 성공    */
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); // 로그인한 유저 정보
+            mDatabase =FirebaseDatabase.getInstance().getReference("Daegu/1");
+            mDatabase.child("user").child(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.getResult().getValue() == null) { /* 회원가입 시  */
+                        User tuser = new User(user.getDisplayName(),0);
+                        mDatabase.child("user").child(user.getUid()).setValue(tuser);
+                        Toast toast = Toast.makeText(getApplicationContext(),"회원가입이 완료되었습니다.",Toast.LENGTH_SHORT);
+                        toast.show();
+                        Intent intent = new Intent(getApplicationContext(), NfcActivity.class);
+                        startActivity(intent);
+                    }
+                    else { /*   로그인    */
+                        Toast toast = Toast.makeText(getApplicationContext(),"로그인이 완료되었습니다.",Toast.LENGTH_SHORT);
+                        toast.show();
+                        Intent intent = new Intent(getApplicationContext(),NfcActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            });
+
             //유저 이름, 유저아이디 디비에 넣기
             //databaseReference.child("user").child().setValue(user.getDisplayName(),user.getUid())
 
 
-            Log.d("LOGIN", "Login success");
             // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
-            Log.d("LOGIN", "Login failed!!");
+        } else {/*  로그인 실패   */
+            Toast toast = Toast.makeText(this.getApplicationContext(),"로그인에 실패하였습니다.",Toast.LENGTH_SHORT);
         }
     }
     @Override
