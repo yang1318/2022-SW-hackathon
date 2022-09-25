c331efab8bb4cf21d57fb314caf63f4f0a52ed20
dahyun0917 <dahyun0917@naver.com>
2022-09-25 13:25:14 +0900

    관광지 별 사진 다르게 설정 성공 및 성공스탬프 사진 수정

--- app/src/main/java/com/example/Colorful_Daegu/control/TouristSpotActivity.java
+++ app/src/main/java/com/example/Colorful_Daegu/control/TouristSpotActivity.java
@@ -199,18 +199,18 @@ public class TouristSpotActivity extends AppCompatActivity {
 //            Glide.with(mCalloutBalloon).load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD6vcHKuBV0F2oiyO9M_8UoqnBlCIFkviLGQ&usqp=CAU").into((imageView));
 //            Glide.with(mCalloutBalloon).load(R.drawable.spot_knu).into((imageView));
             ImageView imgBall = (ImageView) mCalloutBalloon.findViewById(R.id.ball_img);
-            switch (poiItem.getTag()){
+            switch (poiItem.getItemName()){
 //                case "경북대학교": imgBall.setImageResource(R.drawable.spot_knu); break;
-                case 3: imgBall.setImageResource(R.drawable.spot_sparkland);break;
-                case 2: imgBall.setImageResource(R.drawable.spot_eworld);break;
-//                case "앞산 전망대" : imgBall.setImageResource(R.drawable.spot_frontm);break;
-//                case "갓바위" : imgBall.setImageResource(R.drawable.spot_godrock);break;
-//                case 4 : imgBall.setImageResource(R.drawable.spot_street);break;
-                case 1 : imgBall.setImageResource(R.drawable.spot_suseongmot);break;
-                case 5 : imgBall.setImageResource(R.drawable.spot_theark);break;
-                case 6 :imgBall.setImageResource(R.drawable.spot_seomunmarket); break;
-//                case "대구 수목원" : imgBall.setImageResource(R.drawable.spot_arboretum);break;
-//                case "대구숲": imgBall.setImageResource(R.drawable.spot_forest);break;
+                case "동성로 스파크": imgBall.setImageResource(R.drawable.spot_sparkland);break;
+                case "이월드": imgBall.setImageResource(R.drawable.spot_eworld);break;
+                case "앞산 전망대" : imgBall.setImageResource(R.drawable.spot_frontm);break;
+                case "갓바위" : imgBall.setImageResource(R.drawable.spot_godrock);break;
+                case "김광석 거리" : imgBall.setImageResource(R.drawable.spot_street);break;
+                case "수성못" : imgBall.setImageResource(R.drawable.spot_suseongmot);break;
+                case "디아크" : imgBall.setImageResource(R.drawable.spot_theark);break;
+                case "서문시장" :imgBall.setImageResource(R.drawable.spot_seomunmarket); break;
+                case "대구 수목원" : imgBall.setImageResource(R.drawable.spot_arboretum);break;
+                case "대구숲": imgBall.setImageResource(R.drawable.spot_forest);break;
                 default: imgBall.setImageResource(R.drawable.spot_knu);break;
 //                case "대구 스타디움" : imgBall.setImageResource(R.drawable.spot_knu);break;
 //                case "대구 미술관" : imgBall.setImageResource(R.drawable.spot_knu);break;
@@ -221,7 +221,7 @@ public class TouristSpotActivity extends AppCompatActivity {
 //                case "비슬산군립공원" : imgBall.setImageResource(R.drawable.spot_knu);break;
 //                case "국립대구박물관" : imgBall.setImageResource(R.drawable.spot_knu);break;
             }
-            ((ImageView) mCalloutBalloon.findViewById(R.id.ball_img)).setImageResource(R.drawable.spot_knu);
+//            ((ImageView) mCalloutBalloon.findViewById(R.id.ball_img)).setImageResource(R.drawable.spot_knu);
             ((TextView) mCalloutBalloon.findViewById(R.id.text_des)).setText(tours.get(poiItem.getTag()).getDescription());
             ((TextView) mCalloutBalloon.findViewById(R.id.rate_achievement)).setText(Integer.toString(achNum));
             ((TextView) mCalloutBalloon.findViewById(R.id.rating_tourist)).setText(String.valueOf(tours.get(poiItem.getTag()).getRating()));
